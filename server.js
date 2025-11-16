8296b42381e68ca14c4905a560752b65545d2ba7
Vasu Sahu <vasu.sahu.8080@gmail.com>
2025-11-16 19:21:56 +0530

    Refactor server port configuration and logging

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import app from './app.js';
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port,${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 4000;
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
